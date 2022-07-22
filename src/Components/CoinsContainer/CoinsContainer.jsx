7e1c5b9f834338120be8c421a6ae54035b4d544a
Vihar <vihar1998x@gmail.com>
2022-07-22 16:04:13 +0530

    + Implement coingecko api + change paddings and margins of som elements

--- src/Components/CoinsContainer/CoinsContainer.jsx
+++ src/Components/CoinsContainer/CoinsContainer.jsx
@@ -1,50 +1,29 @@
 import React from 'react'
 import "../CoinsContainer/CoinsContainer.css"
-export default function CoinsContainer() {
-  return (
-    <div className='coins-container' >
-        <div className='coin-card' >
-            <img src="../images/bc.png" alt="" />
-            <div className='coin-card-text' > 
-              <h1>
-              $12345
-              </h1>
-              <h2>
-                Bitcoin
-              </h2>
-              <h3 className='won' >
-                Last 24h: -23456%
-              </h3>
-            </div>
+export default function CoinsContainer({ coinList }) {
+
+
+    return (
+        <div className='coins-container' >
+            {coinList.map((coin) => {
+                let change = coin.market_data.price_change_percentage_24h <= 0 ? "down" : "up"
+
+                return <div key={coin.id} className='coin-card' >
+                    <img src={coin.image.large} alt="" />
+                    <div className='coin-card-text' >
+                        <h1>
+                            ${coin.market_data.current_price.usd}
+                        </h1>
+                        <h2>
+                            {coin.name}
+                        </h2>
+                        <h3 className={change} >
+                            Last 24h: {coin.market_data.price_change_percentage_24h}%
+                        </h3>
+                    </div>
+                </div>
+            }
+            )}
         </div>
-        <div className='coin-card' >
-            <img src="../images/eth.png" alt="" />
-            <div className='coin-card-text' > 
-              <h1>
-              $12345
-              </h1>
-              <h2>
-                Bitcoin
-              </h2>
-              <h3 className='won' >
-                Last 24h: -23456%
-              </h3>
-            </div>
-        </div>
-        <div className='coin-card' >
-            <img src="../images/dc.png" alt="" />
-            <div className='coin-card-text' > 
-              <h1>
-              $12345
-              </h1>
-              <h2>
-                Bitcoin
-              </h2>
-              <h3 className='won' >
-                Last 24h: -23456%
-              </h3>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
