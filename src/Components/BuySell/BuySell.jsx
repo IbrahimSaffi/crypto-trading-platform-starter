43280b7aeb34df62a9187de7cd9f84a50bf31a4e
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-07-22 15:35:25 +0500

    commit

--- src/Components/BuySell/BuySell.jsx
+++ src/Components/BuySell/BuySell.jsx
@@ -1,27 +1,47 @@
 import React from 'react'
 import "./BuySell.css"
 export default function BuySell() {
+  // let amount = 0
   return (
     <div className='buy-sell' >
         {/* State component */}
         <div className='buy-sell-label' >
         <h1>Sell Dogecoin</h1>
-        <button>X</button>
+        <button onClick={()=>{
+          // dispatch({type:"brightness"})
+        }}  className='rem' >X</button>
         </div>
         <div className='sub-container' >
 
         <h2>Current Price $1234</h2>
         <div className='input-capacity' >
-            <input type="text" />
+            <input onChange={((e)=>{
+              // amount = e.target.value
+
+            }
+              )} type="number" />
             <h2>Max :50</h2>
         </div>
         <p>You will recieve $10</p>
-        <div className='buy' >
-        <input type="radio" name="buy" id="" />
+        <div className='buy-input' >
+        <input onClick={()=>{
+          // dipatch({type:"transactionType",payload:"buy"})
+          }} type="radio" name="transaction" id="" />
          <h2>Buy</h2>
         </div>
-        <input type="radio" name="sell" id="" />
+        <div className='sell-input' >
+        <input onClick={()=>{
+        // dipatch({type:"transactionType",payload:"sell"})
+        }
+      }
+         type="radio" name="transaction" id="" />
         <h2>Sell</h2>
+        </div>
+        <button onClick={()=>{
+          // dispatch({type:"transactionAmount",payload:amount})
+        }}  className='buy-btn' >
+          Buy
+        </button>
         </div>
     </div>
   )
