50fc80b713344b2d840974fc1242df2adf0a0b04
Vihar <vihar1998x@gmail.com>
2022-07-22 14:12:43 +0530

    Alignment changes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,29 +8,29 @@ import TransactionCard from './TransactionCard/TransactionCard';
 function App() {
     return (
         <div className="app">
-           <div className='main-container' >
-            <BuySell/>
-            <DrescriptionContainer/>
-            <CoinsContainer/>
-            
-            <div className='holdings-transactions'>
-                <div className="holdings">
-                    <h2>Current Holdings</h2>
-                    <p>Go buy some</p>
-                    <div className="holdings-list">
-                        <HoldingCard />
-                    </div>
+            <div className='main-container' >
+                <BuySell />
+                <DrescriptionContainer />
+                <CoinsContainer />
 
-                </div>
-                <div className="transactions">
-                    <h2>Transactions</h2>
-                    <p>No transactions yet...</p>
-                    <div className="holdings-list">
-                        <TransactionCard/>
+                <div className='holdings-transactions'>
+                    <div className="holdings">
+                        <h2>Current Holdings</h2>
+                        <p>Go buy some</p>
+                        <div className="holdings-list">
+                            <HoldingCard />
+                        </div>
+
+                    </div>
+                    <div className="transactions">
+                        <h2>Transactions</h2>
+                        <p>No transactions yet...</p>
+                        <div className="holdings-list">
+                            <TransactionCard />
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>  
         </div>
     );
 }
